3be1658addb57159454cdf9e0f8e7ad89b4b1049
Ahmad Habib afif Abdurrohman (2003015046) <dirosah.ilmahdi@gmail.com>
2024-11-29 13:37:53 +0700

    side information

--- app/Http/Controllers/fullCalenderController.php
+++ app/Http/Controllers/fullCalenderController.php
@@ -3,13 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Models\event;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class fullCalenderController extends Controller
 {
     //
     public function first() {
         return redirect()->route('index');
+    }
+
+    public function dashboard(){
+        $current_year = Carbon::now()->year;
+        $current_month = Carbon::now()->month;
+
+    
+        $events = event::select(
+            DB::raw('DATE(start) as date_start'),
+            DB::raw('TIME(start) as time_start'),
+            DB::raw('DATE(end) as date_end'),
+            DB::raw('TIME(end) as time_end'),
+            'title'
+        )->whereYear('start', $current_year)->whereMonth('start', $current_month)->get();
+        return view('dashboard', compact('events'));
+
     }
     public function index(Request $request)
     {
